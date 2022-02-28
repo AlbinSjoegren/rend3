35d884ed76faf44b92042f536033540a499e8564
Albin Sjögren <albin@sjoegren.se>
2022-02-28 06:45:22 +0100

    initial commit

---
+++ rend3-routine/src/bloom.rs
@@ -0,0 +1,8 @@
+use wgpu::SamplerDescriptor;
+
+pub fn bloom(surface_texture: wgpu::SurfaceTexture, texture_view_descriptor: wgpu::TextureViewDescriptor threshold: f32, size: f32) -> wgpu::SurfaceTexture {
+        let texture = surface_texture.texture;
+        let texture_2 = texture.;
+
+
+}
